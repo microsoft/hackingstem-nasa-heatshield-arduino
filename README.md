ae592b7cee36bce3acac30bc11fab9c8224413d8
Adi Azulay <adazulay@microsoft.com>
2019-06-10 15:16:28 -0700

    Code Review

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Introduction
-Arduino code for the Using materials science engineering to determine heat resistance lesson plan from Hacking STEM.
+Arduino code for the Hacking STEM 'Heat Shield' lesson plan.
 
 # Getting Started
 1. Download lesson assets at http://aka.ms/hackingSTEM
-1. Build your Heat Shield!
-1. Use [Arduino IDE](https://www.arduino.cc/en/Main/Software) to flash the .ino to your [Arduino Uno](https://store.arduino.cc/usa/arduino-uno-rev3) microcontroller
-1. Verify data interactions in Excel
-1. Ready, Set, Science!
+2. Build your Heat Shield!
+3. Use [Arduino IDE](https://www.arduino.cc/en/Main/Software) to flash the .ino to your [Arduino Uno](https://store.arduino.cc/usa/arduino-uno-rev3) microcontroller
+4. Verify data interactions in Excel
+5. Ready, Set, Science!
 
 # Contributing
 We gladly accept pull requests for enhancements and bug fixes! You may also submit feature requests and bugs!
 https://github.com/TODO
 
-# Make it your!
+# Make it yours!
 This project is licensed under the MIT open source license, see License.txt. The MIT license allows you to take this project and make awesome things with it! MIT is a very permissive license, but does require you include license and copyright from License.txt in any derivative work for sake of attribution.
 
 Fork away! Let us know what you build!
--- Thermistor/Thermistor.ino
+++ Thermistor/Thermistor.ino
@@ -6,8 +6,8 @@
 // be found by visiting the Arduino website: 
 // https://www.arduino.cc/en/main/arduinoBoardUno 
 //  
-// This project reads the resistance of 2 thermistors and converts that value 
-// to temprature using the stienhart-hart formula. Each of the thermistors is 
+// This project reads the resistance of two thermistors and converts that value 
+// to temperature using the stienhart-hart formula. Each of the thermistors is
 // setup with a voltage divider, so that we can use the voltage divider 
 // equation to accurately measure the resistance.
 //
@@ -30,47 +30,22 @@
 const int kMaterialThermPin = A0;
 const int kHairDryerThermPin = A1;
 
-// Serial data variables ------------------------------------------------------
-const byte kNumberOfChannelsFromExcel = 6; //Number of channels coming in from Excel
-// IMPORTANT: This must be equal to number of channels set in Data Streamer
-
 const char kDelimiter = ',';    // Data Streamer expects a comma delimeter
 const int kSerialInterval = 1000;   // Interval between serial writes
 unsigned long serialPreviousTime; // Timestamp to track serial interval
 
-char* arr[kNumberOfChannelsFromExcel]; //Array of data coming in from Excel
-
-void setup(void) {
+void setup(void)
+{
   Serial.begin(9600);
-  //analogReference(EXTERNAL);
 }
  
-void loop(void) {
-  processIncomingSerial();
-  if (strcmp("#paused", arr[0]) != 0){
-    sendDataToSerial();
-  } 
+void loop(void)
+{
+  sendDataToSerial();
 }
 
-float getResistance (int pin){
-  //TODO: Test out sampling for data quality
-//   const int kNumSamples = 5;
-//   int samples[kNumSamples];
-
-//   // take N samples in a row, with a slight delay
-//   for (int i=0; i< kNumSamples; i++) {
-//    samples[i] = analogRead(pin);
-//    delay(10);
-//   }
- 
-//   // average all the samples out
-//   float average = 0;
-//   for (int i=0; i< kNumSamples; i++) {
-//      average += samples[i];
-//   }
-//   average /= kNumSamples;
-// //  float reading = average;
-
+float getResistance(int pin)
+{
     const int kSeriesResistor = 10000;
     //Read an analog pin and convert the value to a resistance value in ohms
     float reading = analogRead(pin);
@@ -80,76 +55,41 @@ float getResistance (int pin){
    
 }
 
-float steinhart (float resistanceReading){
+float getTemperature(float resistanceReading)
+{
     // Resistance at 25 C
     const int kThermistorNominal = 10000;
     // Tempatrature for nominal resistance
-    const int kTempratureNominal = 25;
+    const int kTemperatureNominal = 25;
     // Beta coefficient of the thermistor
     const int kBetaCoefficient = 3950;
 
-    float temprature;
-    temprature = resistanceReading / kThermistorNominal;     // (R/Ro)
-    temprature = log(temprature);                  // ln(R/Ro)
-    temprature /= kBetaCoefficient;                   // 1/B * ln(R/Ro)
-    temprature += 1.0 / (kTempratureNominal + 273.15); // + (1/To)
-    temprature = 1.0 / temprature;                 // Invert
-    temprature -= 273.15;                         // convert to C
-    return temprature;
+    // The stienhart-hart formula below is fairly complex for more info see:
+    // https://en.wikipedia.org/wiki/Thermistor
+    float value;
+    value = resistanceReading / kThermistorNominal;
+    value = log(value);
+    value /= kBetaCoefficient;
+    value += 1.0 / (kTemperatureNominal + 273.15);
+    value = 1.0 / value;
+    value -= 273.15;
+    float temperature = value;
+    return temperature;
 }
 
 // OUTGOING SERIAL DATA PROCESSING CODE----------------------------------------
 void sendDataToSerial()
 {
-  float materialResistanceReading = getResistance(kMaterialThermPin);
-  float materialTempReading = steinhart(materialResistanceReading);
-  float hairDryerResistanceReading = getResistance(kHairDryerThermPin);
-  float hairDryerTempReading = steinhart(hairDryerResistanceReading);
-
   // Enter into this only when serial interval has elapsed
   if((millis() - serialPreviousTime) > kSerialInterval) 
   {
     serialPreviousTime = millis(); // Reset serial interval timestamp
-
+    float materialTempReading = getTemperature(getResistance(kMaterialThermPin));
+    float hairDryerTempReading = getTemperature(getResistance(kHairDryerThermPin));
     Serial.print(materialTempReading);
     Serial.print(kDelimiter);
     Serial.print(hairDryerTempReading);
     
     Serial.println(); // Add final line ending character only once
   }
-}
-
-//-----------------------------------------------------------------------------
-// DO NOT EDIT ANYTHING BELOW THIS LINE
-//-----------------------------------------------------------------------------
-
-// INCOMING SERIAL DATA PROCESSING CODE----------------------------------------
-void processIncomingSerial()
-{
-  if(Serial.available()){
-    parseData(GetSerialData());
-  }
-}
-
-// Gathers bytes from serial port to build inputString
-char* GetSerialData()
-{
-  static char inputString[64]; // Create a char array to store incoming data
-  memset(inputString, 0, sizeof(inputString)); // Clear the memory from a pervious reading
-  while (Serial.available()){
-    Serial.readBytesUntil('\n', inputString, 64); //Read every byte in Serial buffer until line end or 64 bytes
-  }
-  return inputString;
-}
-
-// Seperate the data at each delimeter
-void parseData(char data[])
-{
-    char *token = strtok(data, ","); // Find the first delimeter and return the token before it
-    int index = 0; // Index to track storage in the array
-    while (token != NULL){ // Char* strings terminate w/ a Null character. We'll keep running the command until we hit it
-      arr[index] = token; // Assign the token to an array
-      token = strtok(NULL, ","); // Conintue to the next delimeter
-      index++; // incremenet index to store next value
-    }
 }
